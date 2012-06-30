8c6bf7c0f09f10ec8c818d955ab712bec0b68c76
Jan <jan@vialink.com.br>
2012-06-29 21:00:59 -0300

    Multiple enhancements.

--- js/jscli.js
+++ js/jscli.js
@@ -1,6 +1,16 @@
+;(function () {
+
+var history = []
+var position = -1
+
+
 // print string to the output
 function print(str) {
-    document.getElementById('output').innerHTML+= str + '\n'
+    if (window.jQuery) {
+        $('#output').append(str)
+    } else {
+        document.getElementById('output').innerHTML+= str + '\n'
+    }
 }
 // clear the output
 function clear() {
@@ -18,8 +28,11 @@ function init() {
     // we should capture tab, enter and maybe some other keys
     document.getElementById('input').addEventListener('keydown', function (e) {
         //http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-        var TABKEY = 9;
-        var ENTERKEY = 13;
+        var TABKEY = 9
+        var ENTERKEY = 13
+        var UPKEY = 38
+        var DOWNKEY = 40
+
         switch(e.keyCode) {
         case TABKEY:
             //TODO: autocomplete
@@ -31,15 +44,35 @@ function init() {
             var expression = this.value
             this.value = ''
             //TODO: parse and execute
-            print(expression)
+            print(jscli.eval(expression))
             // autoscroll output area
             output.scrollTop = output.scrollHeight - output.clientHeight
             // prevent inputing \n
+            history.push(expression)
+            position++
+            e.preventDefault()
+            break
+        case UPKEY:
+            this.value = history[position--]
+            e.preventDefault()
+            break
+        case DOWNKEY:
+            position++
+            this.value = history[position]
             e.preventDefault()
             break
         }
     }, false)
 }
+
 // we should init when window loads
 window.addEventListener('load', init)
+
+window.jscli = {
+    print: print,
+    clear: clear,
+    eval: function(_) {return _},
+}
+
+})()
 
